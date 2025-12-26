4d1cd70fb9248553a377e61c7de55985f9d1d85b
wholeomarfaruk <wholeomarfaruk@gmail.com>
2025-12-27 01:53:23 +0600

    update

--- resources/views/livewire/sales-point.blade.php
+++ resources/views/livewire/sales-point.blade.php
@@ -495,7 +495,8 @@
                                                    d="m8.25 7.5.415-.207a.75.75 0 0 1 1.085.67V10.5m0 0h6m-6 0h-1.5m1.5 0v5.438c0 .354.161.697.473.865a3.751 3.751 0 0 0 5.452-2.553c.083-.409-.263-.75-.68-.75h-.745M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                            </svg>
 
-                                           <p class="text-sm whitespace-nowrap">{{ $activeInvoice?->customer && $activeInvoice->customer->balance }}
+                                           <p class="text-sm whitespace-nowrap">
+                                               {{ $activeInvoice?->customer && $activeInvoice->customer->balance }}
                                            </p>
                                            {{-- <button wire:click="getCustomerBal({{ $activeInvoice?->customer && $activeInvoice->customer_id }})"
                                                class="cursor-pointer">
@@ -519,7 +520,8 @@
                                            </svg>
 
 
-                                           <p class="text-sm whitespace-nowrap">{{ $activeInvoice?->customer && $activeInvoice->customer->balance }}
+                                           <p class="text-sm whitespace-nowrap">
+                                               {{ $activeInvoice?->customer && $activeInvoice->customer->balance }}
                                            </p>
                                            {{-- <button wire:click="getCustomerBal({{ $activeInvoice?->customer && $activeInvoice?->customer_id }})"
                                                class="cursor-pointer">
@@ -676,16 +678,25 @@
                                    <tbody class="divide-y divide-gray-200 *:even:bg-gray-50">
                                        <tr class="*:text-gray-900 *:first:font-medium">
 
-                                           <th class="px-3 py-2 text-start whitespace-nowrap">Sub Total</th>
+                                           <th class="px-3 py-2 text-start whitespace-nowrap cursor-pointer hover:text-gray-900 hover:bg-emerald-200">Sub Total
+                                               <span class="ml-1">
+                                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                       viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                       class="size-4">
+                                                       <path stroke-linecap="round" stroke-linejoin="round"
+                                                           d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                   </svg>
+                                               </span>
+                                           </th>
                                            <td class="px-3 py-2 text-end whitespace-nowrap">
                                                {{ $activeInvoice->total }}</td>
                                        </tr>
 
-                                       {{-- <tr class="*:text-gray-900 *:first:font-medium">
+                                       <tr class="*:text-gray-900 *:first:font-medium">
                                            <th class="px-3 py-2 text-start whitespace-nowrap">Discount</th>
                                            <td class="px-3 py-2 text-end whitespace-nowrap">
-                                               {{ $activeInvoice->discount_amount }}</td>
-                                       </tr> --}}
+                                               {{ $activeInvoice->discount }}</td>
+                                       </tr>
                                        <tr class="*:text-gray-900 *:first:font-medium">
                                            <th class="px-3 py-2 text-start whitespace-nowrap">Paid</th>
                                            <td class="px-3 py-2 text-end whitespace-nowrap">
@@ -1015,18 +1026,18 @@
                                </div>
                                <div class="flex justify-center items-center gap-2 my-2">
 
-                                @if($settledinvoiceId)
-                                   <a href="{{ route('company.invoice.download', $settledinvoiceId) }}"
-                                       class="inline-flex items-center gap-2 rounded-sm border border-gray-600 px-8 py-3 text-gray-600 hover:bg-gray-600 hover:text-white"
-                                       href="#">
-                                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                           stroke-width="1.5" stroke="currentColor" class="size-6 mr-0.5">
-                                           <path stroke-linecap="round" stroke-linejoin="round"
-                                               d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z" />
-                                       </svg>
-                                       <span class="text-sm font-medium"> Print Invoice </span>
-                                   </a>
-                                @endif
+                                   @if ($settledinvoiceId)
+                                       <a href="{{ route('company.invoice.download', $settledinvoiceId) }}"
+                                           class="inline-flex items-center gap-2 rounded-sm border border-gray-600 px-8 py-3 text-gray-600 hover:bg-gray-600 hover:text-white"
+                                           href="#">
+                                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                               stroke-width="1.5" stroke="currentColor" class="size-6 mr-0.5">
+                                               <path stroke-linecap="round" stroke-linejoin="round"
+                                                   d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z" />
+                                           </svg>
+                                           <span class="text-sm font-medium"> Print Invoice </span>
+                                       </a>
+                                   @endif
                                </div>
                            </div>
                        @else
