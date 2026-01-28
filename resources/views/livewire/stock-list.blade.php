99df962ffe7610c27dea7cf00c834502978523c0
wholeomarfaruk <wholeomarfaruk@gmail.com>
2026-01-28 17:06:22 +0600

    update

--- resources/views/livewire/stock-list.blade.php
+++ resources/views/livewire/stock-list.blade.php
@@ -160,7 +160,7 @@
                                            <span
                                                class="inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700 dark:bg-emerald-700 dark:text-emerald-100">
                                                <svg xmlns="http://www.w3.org/2000/svg" class="size-4 mr-1"
-                                                   fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                   fill="none" viewBox="0 0 24 24" stroke-width="2"
                                                    stroke="currentColor">
                                                    <path stroke-linecap="round" stroke-linejoin="round"
                                                        d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
@@ -170,12 +170,11 @@
                                        @elseif($product->stock_status == \App\Enums\Product\StockStatus::STOCK_OUT->value)
                                            <span
                                                class="inline-flex items-center justify-center rounded-full bg-red-100 px-2.5 py-0.5 text-red-700 dark:bg-red-700 dark:text-red-100">
-
-                                               <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                   viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                   class="size-4 mr-1">
+                                               <svg xmlns="http://www.w3.org/2000/svg" class="size-4 mr-1"
+                                                   fill="none" viewBox="0 0 24 24" stroke-width="2"
+                                                   stroke="currentColor">
                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                       d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+                                                       d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                                                </svg>
 
                                                <p class="text-sm whitespace-nowrap">Stock Out</p>
@@ -184,11 +183,11 @@
                                            <span
                                                class="inline-flex items-center justify-center rounded-full bg-red-100 px-2.5 py-0.5 text-red-700 dark:bg-red-700 dark:text-red-100">
 
-                                               <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                   viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                   class="size-4 mr-1">
+                                              <svg xmlns="http://www.w3.org/2000/svg" class="size-4 mr-1"
+                                                   fill="none" viewBox="0 0 24 24" stroke-width="2"
+                                                   stroke="currentColor">
                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                       d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+                                                       d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                                                </svg>
 
                                                <p class="text-sm whitespace-nowrap">Low Stock</p>
